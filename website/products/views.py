c879070e1d206f2efa00a828ce587e2a73587ea5
fathima mp <fathimamp37@gmail.com>
2024-08-22 00:25:23 +0530

    set layout and index page

--- website/products/views.py
+++ website/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+#list all products
+def list_products(request):
+    return render(request,'list_products.html')
+
+#details of single product
+def product_details(request):
+    return render(request,'product_details.html')
